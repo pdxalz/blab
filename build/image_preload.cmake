d616c1399c61b61eefe55c6e241852891625b994
pdxalz <pdxalz@users.noreply.github.com>
2024-12-27 08:51:18 -0800

    Initial commit

---
+++ build/image_preload.cmake
@@ -0,0 +1,9 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52dk_nrf52832" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "c:/users/pdxal/projects/blab;C:/ncs/v2.2.0/nrf;C:/ncs/v2.2.0/zephyr" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "C:/Users/pdxal/projects/blab/prj.conf;C:/Users/pdxal/projects/blab/boards/nrf52dk_nrf52832.conf" CACHE INTERNAL "NCS child image controlled")
+set(CLI_CONFIG_DEBUG_OPTIMIZATIONS "y" CACHE INTERNAL "NCS child image controlled")
+set(CLI_CONFIG_DEBUG_THREAD_INFO "y" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "C:/Users/pdxal/projects/blab/boards/nrf52dk_nrf52832.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:\ncs\toolchains\v2.2.0\opt\bin\python.exe" CACHE INTERNAL "NCS child image controlled")
